9de5aba688349f415def2aae8ae2bcf9331c9dab
Alan2277 <121243339+Alan2277@users.noreply.github.com>
2023-04-17 14:22:10 -0300

    Create Atividade reforco02.dart

---
+++ Atividade reforco02.dart
@@ -0,0 +1,42 @@
+//class//
+class Produto {
+  List<String> _lista_produtos = [];
+
+  void adicionar_produto(String produto) {
+    this._lista_produtos.add(produto);
+  }
+
+  void remover_produto(String produto) {
+    this._lista_produtos.remove(produto);
+  }
+
+  void produtos_lista() {
+    for (int i = 0; i < this._lista_produtos.length; i++) {
+      print("Codigo = $i \nProduto = ${this._lista_produtos[i]}\n");
+    }
+  }
+
+  get lista_produtos {
+    print("${this._lista_produtos}");
+  }
+}
+
+//execução//
+import 'cod.dart';
+
+void main() {
+  Produto p1 = Produto();
+
+  p1.adicionar_produto("Chinelo");
+  p1.adicionar_produto("Sapato");
+  p1.adicionar_produto("Camisa");
+  p1.adicionar_produto("Bermuda");
+  p1.adicionar_produto("Tênis");
+
+  p1.remover_produto("Camisa");
+  p1.remover_produto("Bermuda");
+
+  p1.produtos_lista();
+  print("A lista de produtos é composta por ${p1.lista_produtos}");
+
+}
